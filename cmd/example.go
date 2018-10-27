ccca47f4fe240c91b441b0ed217e66418e5494fa
QuantumGhost <obelisk.reg+github@gmail.com>
2018-10-27 10:15:12 +0800

    fix: fix type errors in cmd/example.go

--- cmd/example.go
+++ cmd/example.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"math/big"
 
 	"github.com/c2h5oh/hide"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-
+	prime := big.NewInt(47)
 	fmt.Println("Random IDs")
 	for i := 0; i < 10; i++ {
 		v := rand.Int31n(1000000)
-		o := hide.Int32Obfuscate(v, nil)
+		o := hide.Int32Obfuscate(v, prime, nil)
 
 		fmt.Printf("%8d -> %10d\n", v, o)
 	}
@@ -22,7 +23,7 @@ func main() {
 	fmt.Println("\nConsecutive IDs")
 	start := rand.Int31n(1000000)
 	for i := start; i < start+10; i++ {
-		o := hide.Int32Obfuscate(i, nil)
+		o := hide.Int32Obfuscate(i, prime, nil)
 
 		fmt.Printf("%8d -> %10d\n", i, o)
 	}
